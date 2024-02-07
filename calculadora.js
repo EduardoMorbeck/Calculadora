d97d359f23d2a678eccd8db6eba984d333b0f3ab
Eduardo Morbeck <eduardomorbeck187@gmail.com>
2024-02-07 06:41:22 -0300

    segundo commit

--- calculadora.js
+++ calculadora.js
@@ -1,4 +1,9 @@
 'use strict'
 const display = document.getElementById('display')
-const numeros = document.querySelector('[id*=tecla]')
-alert(numeros)
+const numeros = document.querySelectorAll('[id*=tecla]')
+const operadores = document.querySelectorAll('[id*=operador]')
+const inserirNumero
+const selecionarOperador
+
+numeros.forEach(numero => addEventListener('click', inserirNumero))
+operadores.forEach(operador => addEventListener('click', selecionarOperador))
